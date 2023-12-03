ba1ded19ed2d63eed2493f1491b06b38d2607f20
LisiCristian <151665348+LisiCristian@users.noreply.github.com>
2023-12-03 14:26:30 +0100

    Update client.java

--- client.java
+++ client.java
@@ -1,32 +1,32 @@
+// Versione finale
 import java.io.*;
 import java.net.*;
+import java.util.Vector;
 
 public class client extends Thread{
 
     static Socket client;
     static BufferedReader in;
     static PrintWriter out;
-    static boolean off=false;
-    
+    static boolean off=false; //variabile di spegnimento del client
+    static Vector<String> nomi= new Vector<String>();
 
     public client(){
-        start();
+        start();    // Avvia il thread per la ricezione dei messaggi
     }
 
     @Override
-    public void run(){  //thread per la ricezione dei messaggi
-        
+    public void run(){  //thread per la ricezione dei messaggi                     
         while (off==false){
             try {
-                System.out.println(in.readLine());
+                System.out.println(in.readLine());  // Visualizza i messaggi ricevuti dal server
             } catch (IOException e) {
                 System.out.println("Errore nella ricezione dal server");
-                spegni();
+                spegni();   // Se si verifica un errore, chiude il client
                 }
         }
 
     }
-
 
 
     public static void main (String[] args) throws IOException{
@@ -35,9 +35,8 @@ public class client extends Thread{
             System.out.println("Client avviato...");
             Socket client;
             client = new Socket("localhost",9999);
-            // creazione stream di output dal socket
+            // Creazione degli stream di input e output dal socket
             out= new PrintWriter(client.getOutputStream(), true);
-            
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
             System.out.println("Client Socket: "+ client);
@@ -45,18 +44,25 @@ public class client extends Thread{
 
 
 
-            // creazione stream di input da tastiera
-            boolean scrivi=true;
+            boolean scrivi=true;    //booleana per la visualizzazione (o non) sul terminale di "Scrivi:"
             BufferedReader syn = new BufferedReader(new InputStreamReader(System.in));
-            boolean first=true;
+            boolean first=true;     //booleana per identificare il primo messaggio inviato (nome)
             while (off==false){   //ciclo fino a quando il client non decide di disconnettersi
-                if ((first==false) && (scrivi==true)) System.out.println("Scrivi: ");
-                else first=false;
-                if (scrivi==false) {
+
+                if ((first==false) && (scrivi==true)) System.out.println("Scrivi: ");   //se non è il primo messaggio inviato visualizza sul terminale "Scrivi:"
+
+                if ((scrivi==false)) {
                     System.out.print("Storico: \n");
                 scrivi=true;
             }
                 String messaggio = syn.readLine();
+                //controllo validità del nome inserito
+                if (first==true) {
+                    if ((!messaggio.trim().isEmpty()) && (!messaggio.contains("/"))) {
+                        scrivi=true;
+                        first=false;
+                    }
+                }
                 out.println(messaggio);
                 if (messaggio.toLowerCase().equals("/storico")) {
                     scrivi=false;
@@ -81,7 +87,9 @@ public class client extends Thread{
                 out.close();
                 if(client!=null) client.close();
                 System.out.println("Client disconnesso.");
-            } catch (IOException e) {/*ignora*/}
+            } catch (IOException e) {
+                //Ignora
+            }
         }
 
    
