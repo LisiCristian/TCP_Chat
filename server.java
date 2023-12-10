222c0d8243c4e7e45ab93ab33b31d2f94b77c2e6
LisiCristian <151665348+LisiCristian@users.noreply.github.com>
2023-12-10 16:09:13 +0100

    Update server.java

--- server.java
+++ server.java
@@ -10,122 +10,107 @@ public class server extends Thread{
     static Vector<String> nomi = new Vector<String>();
     BufferedReader in;  //ricezione messaggi
     PrintWriter out;    //output sul client
-    static int id=0;
+    Socket client;
 
     public server(Socket client){
-        connessioni.addElement(client);
-       try{
-        out=new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())),true);
-        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-       }catch (IOException e) {}
+        this.client = client;
+        //connessioni.addElement(client);
+        try{
+            out=new PrintWriter(new BufferedWriter(new OutputStreamWriter(this.client.getOutputStream())),true);
+            in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+        }catch (IOException e) { System.out.println(e); }
         start();    // Avvio del thread
     }
 
         @Override
         public void run() {     //thread che gestisce l'invio dei messaggi di ogni client
-            String nome;
-            Socket client;
-            boolean chiudi=false;
-            int myId = id++;    // Memorizza l'id del thread corrente e incrementiamo la variabile id 
-            client = connessioni.elementAt(myId);   //prendiamo dal vettore il client che invia il messaggio
-            try{ 
-
+            String nome = null;
+            try{
                 nome = in.readLine();
-               
                 while (nomi.contains(nome)||(nome.trim().isEmpty())||(nome.contains("/"))){
                     if (nomi.contains(nome)) out.println("usato");
                     else if ((nome.trim().isEmpty())||(nome.contains("/"))) out.println("invalido");
                     nome=in.readLine();
-                    }  
+                }  
                 out.println("true");
-                    
-
+                connessioni.add(client); //una volta effettuato l'accesso possiamo memorizzare il client nella lista dei socket    
                 nomi.addElement(nome);
+
                 System.out.println(nome + " connesso");
-                broadcast(nome+" connesso",client);
-                while (chiudi==false){  //ciclo fino a quando il client non si disconnette
+                
+                broadcast( (nome+" connesso"), client);
+                while (true){  //ciclo fino a quando il client non si disconnette
                     String messaggio= in.readLine();
-                    if (messaggio.toLowerCase().equals("/esci")){
-                        chiudi=true;
-                        break;
-                    }
-                    if (messaggio.toLowerCase().equals("/storico")){
-                        storico(out);
-                    }
+                    if (messaggio.toLowerCase().equals("/esci")) break;
+                    
+                    else if (messaggio.toLowerCase().equals("/storico")) storico(out);
                     else{
-                        messaggio= nome + ": " + messaggio;
-                        broadcast (messaggio,client);
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss dd/MM/yyyy").withZone(ZoneId.of("Europe/Rome")).withLocale(Locale.ITALY);
-                        LocalDateTime dataora = LocalDateTime.of(LocalDate.now(), LocalTime.now());
-                        if(!messaggio.toLowerCase().equals("/storico"))  salvataggio(messaggio +" <"+dataora.format(formatter)+">");
+                        if(!messaggio.startsWith("/")){
+                            messaggio = nome + ": " + messaggio;
+                            broadcast (messaggio,client);
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss dd/MM/yyyy").withZone(ZoneId.of("Europe/Rome")).withLocale(Locale.ITALY);
+                            LocalDateTime dataora = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+                            //memorizza tutti i messaggi che non iniziano con "/"
+                            salvataggio(messaggio +" <"+dataora.format(formatter)+">");
+                        }
                     } 
                 }
-                if (chiudi==true) {
-                    System.out.println(nome+" disconnesso");
-                    broadcast(nome+" ha lasciato la chat",client);
-                    chiudiClient(client,in,out,myId);
-                }
-            }catch(Exception e) {
-                chiudiClient(client,in,out,myId);
+                System.out.println(nome+" disconnesso");
+                broadcast(nome+" ha lasciato la chat",client);
+                chiudiClient(client,in,out, nome);
+            }catch(SocketException e) { 
+                chiudiClient(client,in,out,nome); 
+                if(nome != null) System.out.println(nome+" si e' disconneesso");
             }
+            catch (IOException e){ System.out.println(e);}
         }
 
-
-        public void uscitaChat (int myId){
-            connessioni.removeElementAt(myId); //Rimuove il client dal vettore delle connessioni 
-            nomi.removeElementAt(myId);
-            id--;   //Decrementa l'id per mantenere la coerenza nel vettore
+        public void uscitaChat (String nome){
+            connessioni.removeElement(client); //Rimuove il client dal vettore delle connessioni 
+            connessioni.trimToSize();
+            nomi.removeElement(nome); //Rimuove il nome dalla lista dei nomi
+            nomi.trimToSize();
         }
-        public void chiudiClient (Socket client, BufferedReader in, PrintWriter out,int myId){
-            uscitaChat(myId);
+        public void chiudiClient (Socket client, BufferedReader in, PrintWriter out, String nome){
+            uscitaChat(nome);
             try{
                 // Chiude gli stream di input e output e il socket del client
                 if (in!=null) in.close();
                 if (out!=null) out.close();
                 if (client!=null) client.close();
-            }catch (Exception e){
-                //ignora
-            }
+            }catch (Exception e){ System.out.println(e);}
         }
 
 
         public static void broadcast (String messaggio, Socket client){
             for (int i=0; i<connessioni.size(); i++){
-                            Socket destinatario= new Socket();
-                            destinatario=connessioni.elementAt(i);
-                            //broadcast a tutti i client eccetto il mittente
-                            if (destinatario!=client) {
-                                try {
-                                    new PrintWriter(destinatario.getOutputStream(),true).println(messaggio);
-                                } catch (IOException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
+                Socket destinatario = connessioni.elementAt(i);
+                //broadcast a tutti i client eccetto il mittente
+                if (destinatario!=client) {
+                    try {
+                        new PrintWriter(destinatario.getOutputStream(),true).println(messaggio);
+                    } catch (IOException e) { System.out.println(e); }
+                }
+            }
         }
 
-
-
-
     public static void main (String [] args) throws IOException{
+        ServerSocket socketBenvenuto = new ServerSocket();
         try {
-            ServerSocket socketBenvenuto = new ServerSocket();
             socketBenvenuto.bind(new InetSocketAddress(InetAddress.getLocalHost(),16999));
             System.out.println("Server in ascolto sulla porta locale di benvenuto: "+ socketBenvenuto.getLocalPort());
             
-            
             while (true) {
-                
-                Socket client = socketBenvenuto.accept();
-                
-                System.out.println("Connessione accettata, socket client: " + client);
-                new server(client);
-            }    
-        } catch (IOException e) { 
-            System.out.println("Accept fallito");
-            System.exit(1);
-            }
+                try {
+                    Socket client = socketBenvenuto.accept();
+                    System.out.println("Connessione accettata, socket client: " + client);
+                    new server(client);
+                } catch (IOException e) { System.out.println("Accept fallito"); }
+            } 
+        } catch (Exception e) { 
+            System.out.println(e);
+            socketBenvenuto.close();
+        }
     }//fine main
 
 
@@ -136,7 +121,7 @@ public class server extends Thread{
             PrintWriter scrivi= new PrintWriter(scrittore);
             scrivi.println(messaggio);
             scrittore.close();
-        }catch (Exception e){}
+        }catch (Exception e){ System.out.println(e); }
     }
 
     
@@ -152,8 +137,6 @@ public class server extends Thread{
             }  
             in.close();
             out.println("\nScrivi: ");      
-        }catch (Exception e){}
+        }catch (Exception e){ System.out.println(e); }
     }
-
-
 }
